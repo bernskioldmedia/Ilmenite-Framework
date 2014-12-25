e26002274a86b42d118cb26da446b8157c5ab475
Erik Bernskiold <erik@bernskioldmedia.com>
2014-12-25 11:13:36 +0100

    Update search form code
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- searchform.php
+++ searchform.php
@@ -13,6 +13,12 @@ if ( isset( $_GET['s'] ) ) {
 }
 ?>
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-	<input type="text" value="<?php echo $value; ?>" name="s" id="s" placeholder="<?php _e('What are you looking for?', 'TEXTDOMAINTHEMENAME'); ?>" />
-	<input type="submit" id="searchsubmit" value="<?php _e('Search', 'TEXTDOMAINTHEMENAME'); ?>" />
+	<div class="row collapse">
+		<div class="small-18 medium-16 large-18 columns">
+			<input type="text" value="<?php echo $value; ?>" name="s" id="s" placeholder="<?php _e( 'What are you looking for?', 'TEXTDOMAINTHEMENAME' ); ?>" />
+		</div>
+		<div class="small-6 medium-8 large-6 columns">
+			<input type="submit" id="searchsubmit" class="button postfix" value="<?php _e( 'Search', 'TEXTDOMAINTHEMENAME' ); ?>" />
+		</div>
+	</div>
 </form>
