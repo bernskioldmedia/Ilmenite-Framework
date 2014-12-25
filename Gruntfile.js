83f9139aa953ed080e6a4751f8411574db7e2bbb
Erik Bernskiold <erik@bernskioldmedia.com>
2014-12-25 10:44:19 +0100

    Add pot generation gruntfile
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,33 @@ module.exports = function(grunt) {
 			}
 		},
 
+		pot: {
+	      options:{
+	          text_domain: 'TEXTDOMAINTHEMENAME', // Your text domain. Produces my-text-domain.pot
+	          dest: 'languages/', //directory to place the pot file
+	          keywords: [ //WordPress localisation functions
+	            '__:1',
+	            '_e:1',
+	            '_x:1,2c',
+	            'esc_html__:1',
+	            'esc_html_e:1',
+	            'esc_html_x:1,2c',
+	            'esc_attr__:1',
+	            'esc_attr_e:1',
+	            'esc_attr_x:1,2c',
+	            '_ex:1,2c',
+	            '_n:1,2',
+	            '_nx:1,2,4c',
+	            '_n_noop:1,2',
+	            '_nx_noop:1,2,3c'
+	           ],
+	      },
+	      files:{
+	          src:  [ '**/*.php' ], //Parse all php files
+	          expand: true,
+	      }
+		},
+
 		autoprefixer: {
          dist: {
             files: {
@@ -59,6 +86,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-pot');
 
 	// Run!
 	grunt.registerTask('default', ['sass', 'uglify', 'watch']);
