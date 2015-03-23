db6ed3aadde862f24bd523dc1a740ee3011b1820
Erik Bernskiold <erik@bernskioldmedia.com>
2015-03-23 21:40:20 +0100

    Title tag now handled by theme support
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- header.php
+++ header.php
@@ -11,8 +11,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-		<title><?php wp_title(''); ?></title>
-
 		<?php wp_head(); ?>
 
 	</head>
