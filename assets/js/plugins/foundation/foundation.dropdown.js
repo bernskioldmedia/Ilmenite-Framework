52eba611b33e7db049bd2b2c072bd1b6a6e41e04
Erik Bernskiold <erik@bernskioldmedia.com>
2015-03-23 19:48:31 +0100

    Update Foundation
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- assets/js/plugins/foundation/foundation.dropdown.js
+++ assets/js/plugins/foundation/foundation.dropdown.js
@@ -4,19 +4,24 @@
   Foundation.libs.dropdown = {
     name : 'dropdown',
 
-    version : '5.2.2',
+    version : '{{VERSION}}',
 
     settings : {
-      active_class: 'open',
-      align: 'bottom',
-      is_hover: false,
-      opened: function(){},
-      closed: function(){}
+      active_class : 'open',
+      disabled_class : 'disabled',
+      mega_class : 'mega',
+      align : 'bottom',
+      pip : 'default',
+      is_hover : false,
+      hover_timeout : 150,
+      opened : function () {},
+      closed : function () {}
     },
 
     init : function (scope, method, options) {
       Foundation.inherit(this, 'throttle');
 
+      $.extend(true, this.settings, method, options);
       this.bindings(method, options);
     },
 
@@ -30,50 +35,84 @@
           var settings = S(this).data(self.attr_name(true) + '-init') || self.settings;
           if (!settings.is_hover || Modernizr.touch) {
             e.preventDefault();
+            if (S(this).parent('[data-reveal-id]')) {
+              e.stopPropagation();
+            }
             self.toggle($(this));
           }
         })
         .on('mouseenter.fndtn.dropdown', '[' + this.attr_name() + '], [' + this.attr_name() + '-content]', function (e) {
-          var $this = S(this);
+          var $this = S(this),
+              dropdown,
+              target;
+
           clearTimeout(self.timeout);
 
           if ($this.data(self.data_attr())) {
-            var dropdown = S('#' + $this.data(self.data_attr())),
-                target = $this;
+            dropdown = S('#' + $this.data(self.data_attr()));
+            target = $this;
+            self.last_target = target;
           } else {
-            var dropdown = $this;
-                target = S("[" + self.attr_name() + "='" + dropdown.attr('id') + "']");
+            dropdown = $this;
+
+            if (self.last_target) {
+              target = self.last_target;
+            } else {
+              target = S("[" + self.attr_name() + "='" + dropdown.attr('id') + "']");
+            }
           }
 
           var settings = target.data(self.attr_name(true) + '-init') || self.settings;
-          
-          if(S(e.target).data(self.data_attr()) && settings.is_hover) {
+
+          if (S(e.currentTarget).data(self.data_attr()) && settings.is_hover) {
             self.closeall.call(self);
           }
-          
-          if (settings.is_hover) self.open.apply(self, [dropdown, target]);
+
+          if (settings.is_hover) {
+            self.open.apply(self, [dropdown, target]);
+          }
         })
         .on('mouseleave.fndtn.dropdown', '[' + this.attr_name() + '], [' + this.attr_name() + '-content]', function (e) {
           var $this = S(this);
+          var settings;
+
+          if ($this.data(self.data_attr())) {
+              settings = $this.data(self.data_attr(true) + '-init') || self.settings;
+          } else {
+              var target   = S('[' + self.attr_name() + '="' + S(this).attr('id') + '"]'),
+                  settings = target.data(self.attr_name(true) + '-init') || self.settings;
+          }
+
           self.timeout = setTimeout(function () {
             if ($this.data(self.data_attr())) {
-              var settings = $this.data(self.data_attr(true) + '-init') || self.settings;
-              if (settings.is_hover) self.close.call(self, S('#' + $this.data(self.data_attr())));
+              if (settings.is_hover) {
+                self.close.call(self, S('#' + $this.data(self.data_attr())));
+              }
             } else {
-              var target = S('[' + self.attr_name() + '="' + S(this).attr('id') + '"]'),
-                  settings = target.data(self.attr_name(true) + '-init') || self.settings;
-              if (settings.is_hover) self.close.call(self, $this);
+              if (settings.is_hover) {
+                self.close.call(self, $this);
+              }
             }
-          }.bind(this), 150);
+          }.bind(this), settings.hover_timeout);
         })
         .on('click.fndtn.dropdown', function (e) {
           var parent = S(e.target).closest('[' + self.attr_name() + '-content]');
+          var links  = parent.find('a');
 
-          if (S(e.target).data(self.data_attr()) || S(e.target).parent().data(self.data_attr())) {
+          if (links.length > 0 && parent.attr('aria-autoclose') !== 'false') {
+              self.close.call(self, S('[' + self.attr_name() + '-content]'));
+          }
+
+          if (e.target !== document && !$.contains(document.documentElement, e.target)) {
             return;
           }
-          if (!(S(e.target).data('revealId')) && 
-            (parent.length > 0 && (S(e.target).is('[' + self.attr_name() + '-content]') || 
+
+          if (S(e.target).closest('[' + self.attr_name() + ']').length > 0) {
+            return;
+          }
+
+          if (!(S(e.target).data('revealId')) &&
+            (parent.length > 0 && (S(e.target).is('[' + self.attr_name() + '-content]') ||
               $.contains(parent.first()[0], e.target)))) {
             e.stopPropagation();
             return;
@@ -82,10 +121,10 @@
           self.close.call(self, S('[' + self.attr_name() + '-content]'));
         })
         .on('opened.fndtn.dropdown', '[' + self.attr_name() + '-content]', function () {
-            self.settings.opened.call(this);
+          self.settings.opened.call(this);
         })
         .on('closed.fndtn.dropdown', '[' + self.attr_name() + '-content]', function () {
-            self.settings.closed.call(this);
+          self.settings.closed.call(this);
         });
 
       S(window)
@@ -97,37 +136,46 @@
       this.resize();
     },
 
-    close: function (dropdown) {
+    close : function (dropdown) {
       var self = this;
-      dropdown.each(function () {
+      dropdown.each(function (idx) {
+        var original_target = $('[' + self.attr_name() + '=' + dropdown[idx].id + ']') || $('aria-controls=' + dropdown[idx].id + ']');
+        original_target.attr('aria-expanded', 'false');
         if (self.S(this).hasClass(self.settings.active_class)) {
           self.S(this)
-            .css(Foundation.rtl ? 'right':'left', '-99999px')
+            .css(Foundation.rtl ? 'right' : 'left', '-99999px')
+            .attr('aria-hidden', 'true')
             .removeClass(self.settings.active_class)
             .prev('[' + self.attr_name() + ']')
-            .removeClass(self.settings.active_class);
+            .removeClass(self.settings.active_class)
+            .removeData('target');
 
-          self.S(this).trigger('closed', [dropdown]);
+          self.S(this).trigger('closed.fndtn.dropdown', [dropdown]);
         }
       });
+      dropdown.removeClass('f-open-' + this.attr_name(true));
     },
 
-    closeall: function() {
+    closeall : function () {
       var self = this;
-      $.each(self.S('[' + this.attr_name() + '-content]'), function() {
-        self.close.call(self, self.S(this))
+      $.each(self.S('.f-open-' + this.attr_name(true)), function () {
+        self.close.call(self, self.S(this));
       });
     },
 
-    open: function (dropdown, target) {
-        this
-          .css(dropdown
-            .addClass(this.settings.active_class), target);
-        dropdown.prev('[' + this.attr_name() + ']').addClass(this.settings.active_class);
-        dropdown.trigger('opened', [dropdown, target]);
+    open : function (dropdown, target) {
+      this
+        .css(dropdown
+        .addClass(this.settings.active_class), target);
+      dropdown.prev('[' + this.attr_name() + ']').addClass(this.settings.active_class);
+      dropdown.data('target', target.get(0)).trigger('opened.fndtn.dropdown', [dropdown, target]);
+      dropdown.attr('aria-hidden', 'false');
+      target.attr('aria-expanded', 'true');
+      dropdown.focus();
+      dropdown.addClass('f-open-' + this.attr_name(true));
     },
 
-    data_attr: function () {
+    data_attr : function () {
       if (this.namespace.length > 0) {
         return this.namespace + '-' + this.name;
       }
@@ -136,6 +184,9 @@
     },
 
     toggle : function (target) {
+      if (target.hasClass(this.settings.disabled_class)) {
+        return;
+      }
       var dropdown = this.S('#' + target.data(this.data_attr()));
       if (dropdown.length === 0) {
         // No dropdown found, not continuing
@@ -146,15 +197,17 @@
 
       if (dropdown.hasClass(this.settings.active_class)) {
         this.close.call(this, dropdown);
+        if (dropdown.data('target') !== target.get(0)) {
+          this.open.call(this, dropdown, target);
+        }
       } else {
-        this.close.call(this, this.S('[' + this.attr_name() + '-content]'))
         this.open.call(this, dropdown, target);
       }
     },
 
     resize : function () {
-      var dropdown = this.S('[' + this.attr_name() + '-content].open'),
-          target = this.S("[" + this.attr_name() + "='" + dropdown.attr('id') + "']");
+      var dropdown = this.S('[' + this.attr_name() + '-content].open');
+      var target = $(dropdown.data("target"));
 
       if (dropdown.length && target.length) {
         this.css(dropdown, target);
@@ -162,21 +215,38 @@
     },
 
     css : function (dropdown, target) {
+      var left_offset = Math.max((target.width() - dropdown.width()) / 2, 8),
+          settings = target.data(this.attr_name(true) + '-init') || this.settings,
+          parentOverflow = dropdown.parent().css('overflow-y') || dropdown.parent().css('overflow');
+
       this.clear_idx();
 
+
+
       if (this.small()) {
-        var p = this.dirs.bottom.call(dropdown, target);
+        var p = this.dirs.bottom.call(dropdown, target, settings);
 
         dropdown.attr('style', '').removeClass('drop-left drop-right drop-top').css({
           position : 'absolute',
-          width: '95%',
-          'max-width': 'none',
-          top: p.top
+          width : '95%',
+          'max-width' : 'none',
+          top : p.top
         });
 
-        dropdown.css(Foundation.rtl ? 'right':'left', '2.5%');
-      } else {
-        var settings = target.data(this.attr_name(true) + '-init') || this.settings;
+        dropdown.css(Foundation.rtl ? 'right' : 'left', left_offset);
+      }
+      // detect if dropdown is in an overflow container
+      else if (parentOverflow !== 'visible') {
+        var offset = target[0].offsetTop + target[0].offsetHeight;
+
+        dropdown.attr('style', '').css({
+          position : 'absolute',
+          top : offset
+        });
+
+        dropdown.css(Foundation.rtl ? 'right' : 'left', left_offset);
+      }
+      else {
 
         this.style(dropdown, target, settings);
       }
@@ -185,7 +255,7 @@
     },
 
     style : function (dropdown, target, settings) {
-      var css = $.extend({position: 'absolute'}, 
+      var css = $.extend({position : 'absolute'},
         this.dirs[settings.align].call(dropdown, target, settings));
 
       dropdown.attr('style', '').css(css);
@@ -203,67 +273,177 @@
         p.top -= o.top;
         p.left -= o.left;
 
+        //set some flags on the p object to pass along
+        p.missRight = false;
+        p.missTop = false;
+        p.missLeft = false;
+        p.leftRightFlag = false;
+
+        //lets see if the panel will be off the screen
+        //get the actual width of the page and store it
+        p.bodyWidth = window.innerWidth;
+        if (document.getElementsByClassName('row')[0]) {
+          p.bodyWidth = document.getElementsByClassName('row')[0].clientWidth;
+        }
+
         return p;
       },
-      top: function (t, s) {
+
+      _position_bottom : function(d,t,s,p) {
+        if(d.outerWidth() > t.outerWidth()) {
+          //miss right
+          if (p.left + d.outerWidth() > p.bodyWidth) {
+            //miss left
+            if(p.left - (d.outerWidth() - t.outerWidth()) < 0) {
+              // set triggered right if the dropdown won't fit inside the first .row
+              // in either the left or right orientation.
+              p.triggeredRight = true;
+              p.missLeft = true;
+            } else {
+              p.missRight = true;
+            }
+          }
+        }
+
+        if (t.outerWidth() > d.outerWidth() && s.pip == 'center') {
+          p.offset = (t.outerWidth() - d.outerWidth()) / 2;
+        }
+        else if (p.triggeredRight) {
+          if(d.outerWidth() < p.bodyWidth) {
+            p.offset = (p.bodyWidth - p.left) - d.outerWidth();
+          } else {
+            p.offset = -p.left;
+          }
+        }
+        else if (p.missRight || self.rtl) {
+          p.offset = -d.outerWidth() + t.outerWidth();
+        } else {
+          p.offset = 0;
+        }
+
+        return p;
+      },
+
+      top : function (t, s) {
         var self = Foundation.libs.dropdown,
             p = self.dirs._base.call(this, t),
-            pip_offset_base = (t.outerWidth() / 2) - 8;
+            offsetTop = -this.outerHeight();
+
+        p = self.dirs._position_bottom(this,t,s,p);
 
         this.addClass('drop-top');
 
-        if (t.outerWidth() < this.outerWidth() || self.small()) {
-          self.adjust_pip(pip_offset_base, p);
+        //miss top
+        if (t.offset().top <= this.outerHeight()) {
+          p.missTop = true;
+          p.leftRightFlag = true;
+          this.removeClass('drop-top');
+          offsetTop = t.outerHeight();
         }
 
-        if (Foundation.rtl) {
-          return {left: p.left - this.outerWidth() + t.outerWidth(), 
-            top: p.top - this.outerHeight()};
+        if (self.rtl || t.outerWidth() < this.outerWidth() || self.small() || this.hasClass(s.mega_menu)) {
+          self.adjust_pip(this,t,s,p);
         }
 
-        return {left: p.left, top: p.top - this.outerHeight()};
+        return {left : p.left + p.offset, top : p.top + offsetTop};
       },
-      bottom: function (t, s) {
+
+      bottom : function (t, s) {
         var self = Foundation.libs.dropdown,
-            p = self.dirs._base.call(this, t),
-            pip_offset_base = (t.outerWidth() / 2) - 8;
+            p = self.dirs._base.call(this, t);
+        
+        p = self.dirs._position_bottom(this,t,s,p);       
 
-        if (t.outerWidth() < this.outerWidth() || self.small()) {
-          self.adjust_pip(pip_offset_base, p);
+        // Is this if statement really worth it?
+        // I assume it is here to avoid unnecessary sheet.insertRule calls, but how expensive are they?
+        if (p.offset || t.outerWidth() < this.outerWidth() || self.small() || this.hasClass(s.mega_menu)) {
+          self.adjust_pip(this,t,s,p);
         }
 
-        if (self.rtl) {
-          return {left: p.left - this.outerWidth() + t.outerWidth(), top: p.top + t.outerHeight()};
-        }
-
-        return {left: p.left, top: p.top + t.outerHeight()};
+        return {left : p.left + p.offset, top : p.top + t.outerHeight()};
       },
-      left: function (t, s) {
-        var p = Foundation.libs.dropdown.dirs._base.call(this, t);
+
+      left : function (t, s) {
+        var self = Foundation.libs.dropdown,
+            p = self.dirs._base.call(this, t);
+        p.offset = -this.outerWidth();
 
         this.addClass('drop-left');
 
-        return {left: p.left - this.outerWidth(), top: p.top};
+        //miss left
+        if (p.left - this.outerWidth() <= 0) {
+          p.missLeft = true;
+          p.missRight = false;
+          p.top = p.top + t.outerHeight();
+          this.removeClass('drop-left');
+          p = self.dirs._position_bottom(this,t,s,p);
+          self.adjust_pip(this,t,s,p);
+        } else {
+          self.adjust_pip_vertical(this,t,s,p);
+        }
+
+        return {left : p.left + p.offset, top : p.top};
       },
-      right: function (t, s) {
-        var p = Foundation.libs.dropdown.dirs._base.call(this, t);
+
+      right : function (t, s) {
+        var self = Foundation.libs.dropdown,
+            p = self.dirs._base.call(this, t);
+
+        p.offset = t.outerWidth();
 
         this.addClass('drop-right');
 
-        return {left: p.left + t.outerWidth(), top: p.top};
+        //miss right
+        if (p.left + this.outerWidth() + t.outerWidth() > p.bodyWidth) {
+          p.missRight = true;
+          p.missLeft = false;
+          p.top = p.top + t.outerHeight();
+          this.removeClass('drop-right');
+          p = self.dirs._position_bottom(this,t,s,p);
+          self.adjust_pip(this,t,s,p);
+        } else {
+          p.triggeredRight = true;
+          self.adjust_pip_vertical(this,t,s,p);
+        }
+
+        var self = Foundation.libs.dropdown;
+
+        if (t.outerWidth() < this.outerWidth() || self.small() || this.hasClass(s.mega_menu)) {
+          self.adjust_pip(this, t, s, p);
+        }
+
+        return {left : p.left + t.outerWidth(), top : p.top};
       }
     },
 
     // Insert rule to style psuedo elements
-    adjust_pip : function (pip_offset_base, p) {
-      var sheet = Foundation.stylesheet;
+    adjust_pip : function (dropdown, target, settings, position) {
+      var sheet = Foundation.stylesheet,
+          pip_offset_base = 8;
 
-      if (this.small()) {
-        pip_offset_base += p.left - 8;
+      if (dropdown.hasClass(settings.mega_class)) {
+        pip_offset_base = position.left + (target.outerWidth()/2) - 8;
+      }
+      else if (this.small()) {
+        pip_offset_base = position.left;
+        if (settings.pip == 'center') {
+          pip_offset_base += (target.outerWidth()/2) - 15;
+        }
+      }
+      else if (settings.pip == 'center') {
+        if(target.outerWidth() < dropdown.outerWidth()){
+          pip_offset_base = (target.outerWidth()/2) - position.offset - 7;
+        } else {
+          pip_offset_base = (dropdown.outerWidth()/2) - 7;
+        }
+      }
+      else if (position.missRight) {
+        pip_offset_base += target.outerWidth() - 30;
       }
 
       this.rule_idx = sheet.cssRules.length;
 
+      //default
       var sel_before = '.f-dropdown.open:before',
           sel_after  = '.f-dropdown.open:after',
           css_before = 'left: ' + pip_offset_base + 'px;',
@@ -278,11 +458,37 @@
       }
     },
 
+    adjust_pip_vertical : function (dropdown,target,settings,position) {
+      var sheet = Foundation.stylesheet,
+          pip_offset_base = 10,
+          pip_halfheight = 14;
+
+      if (settings.pip == 'center') {
+        pip_offset_base = (target.outerHeight() - pip_halfheight) / 2;
+      }
+
+      this.rule_idx = sheet.cssRules.length;
+
+      //default
+      var sel_before = '.f-dropdown.open:before',
+          sel_after  = '.f-dropdown.open:after',
+          css_before = 'top: ' + pip_offset_base + 'px;',
+          css_after  = 'top: ' + (pip_offset_base - 1) + 'px;';
+        
+      if (sheet.insertRule) {
+        sheet.insertRule([sel_before, '{', css_before, '}'].join(' '), this.rule_idx);
+        sheet.insertRule([sel_after, '{', css_after, '}'].join(' '), this.rule_idx + 1);
+      } else {
+        sheet.addRule(sel_before, css_before, this.rule_idx);
+        sheet.addRule(sel_after, css_after, this.rule_idx + 1);
+      }
+    },
+
     // Remove old dropdown rule index
     clear_idx : function () {
       var sheet = Foundation.stylesheet;
 
-      if (this.rule_idx) {
+      if (typeof this.rule_idx !== 'undefined') {
         sheet.deleteRule(this.rule_idx);
         sheet.deleteRule(this.rule_idx);
         delete this.rule_idx;
@@ -294,7 +500,7 @@
         !matchMedia(Foundation.media_queries.medium).matches;
     },
 
-    off: function () {
+    off : function () {
       this.S(this.scope).off('.fndtn.dropdown');
       this.S('html, body').off('.fndtn.dropdown');
       this.S(window).off('.fndtn.dropdown');
