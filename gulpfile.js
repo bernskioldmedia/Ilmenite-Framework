0af1c6976617c0a2c990fc3c88ac869258b45085
Erik Bernskiold <erik@bernskioldmedia.com>
2015-08-28 17:10:37 +0200

    Add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,10 @@ gulp.task('sass', function () {
     return gulp.src('assets/scss/layout.scss')
     	.pipe(plumber(plumberErrorHandler))
     	.pipe(sass({outputStyle: 'compressed'}))
+    	.pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('assets/css'))
         .pipe(livereload());
 
