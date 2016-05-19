ae2e00bf760968c03db25b11c698bc4d29c206b3
Erik Bernskiold <erik@bernskioldmedia.com>
2016-05-19 21:57:44 +0200

    Textdomain

--- includes/class-theme.php
+++ includes/class-theme.php
@@ -170,7 +170,7 @@ class Ilmenite_Theme {
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on Ilmenite, use a find and replace
-		 * to change 'ilmenite' to the name of your theme in all the template files
+		 * to change 'THEMETEXTDOMAIN' to the name of your theme in all the template files
 		 */
 		load_theme_textdomain( 'THEMETEXTDOMAIN', get_template_directory() . '/languages' );
 
