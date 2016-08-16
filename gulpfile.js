c34a39fe45c06b44e79341be59ca885041d1937d
Erik Bernskiold <erik@bernskioldmedia.com>
2016-08-16 16:45:13 +0200

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp 			= require('gulp');
 var autoprefixer 	= require('gulp-autoprefixer');
 var concat 			= require('gulp-concat');
+var imagemin 		= require('gulp-imagemin'),
 var jshint 			= require('gulp-jshint');
 var livereload 		= require('gulp-livereload');
 var notify 			= require('gulp-notify');
@@ -18,40 +19,69 @@ var plumberErrorHandler = { errorHandler: notify.onError({
   })
 };
 
-// SASS
-gulp.task('sass', function () {
+// Styles
+gulp.task('styles', function() {
 
-    return gulp.src('assets/scss/main.scss')
-    	.pipe(plumber(plumberErrorHandler))
-    	.pipe(sass({outputStyle: 'compressed'}))
-    	.pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('assets/css'))
-        .pipe(livereload());
+	return gulp.src('assets/scss/main.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}))
+		.pipe(autoprefixer({
+			browsers: [
+				'last 2 versions'
+			],
+			cascade: false
+		}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('assets/css'))
+		.pipe(livereload());
+		.pipe(notify({
+			message: 'Styles Task Completed!'
+		}));
 
 });
 
-// JavaScript
-gulp.task('js', function () {
+// Scripts
+gulp.task('scripts', function() {
 
-	return gulp.src([
+	var scripts = [
 		'assets/js/src/plugins/iconic.min.js',
 		'assets/js/src/plugins/foundation/foundation.core.js',
 		'assets/js/src/plugins/foundation/foundation.util.*.js',
 		'assets/js/src/main.js'
-	])
-		.pipe(plumber(plumberErrorHandler))
-		.pipe(sourcemaps.init())
-		.pipe(concat("theme.js"))
-		.pipe(gulp.dest("assets/js"))
-		.pipe(rename("theme.min.js"))
-		.pipe(uglify())
-		.pipe(sourcemaps.write("./"))
-		.pipe(gulp.dest("assets/js"))
-		.pipe(livereload());
+	];
 
+	return gulp.src(scripts)
+		.pipe(sourcemaps.init())
+		.pipe(concat('theme.js'))
+		.pipe(gulp.dest('assets/js'))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('assets/js'))
+		.pipe(livereload())
+		.pipe(notify({
+			message: 'Scripts Task Completed!'
+		}));
+
+});
+
+// Images
+gulp.task('images', function() {
+  return gulp.src('assets/images/**/*')
+    .pipe(cache(imagemin({
+    	optimizationLevel: 3,
+    	progressive: true,
+    	interlaced: true
+    })))
+    .pipe(livereload())
+    .pipe(gulp.dest('assets/images'))
+    .pipe(notify({
+    	message: 'Images Task Completed'
+    }));
 });
 
 // Watch...
@@ -59,10 +89,25 @@ gulp.task('watch', function() {
 
 	livereload.listen();
 
-	gulp.watch('assets/scss/**/*.scss', ['sass']);
+	// Watch .scss files
+	gulp.watch('assets/scss/**/*.scss', function(event) {
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+		gulp.run('styles');
+	});
 
-	gulp.watch('assets/js/src/*.js', ['js']);
+	// Watch .js files
+	gulp.watch('assets/js/src/**/*.js', function(event) {
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+		gulp.run('scripts');
+	});
+
+	// Watch image files
+	gulp.watch('assets/images/**/*', function(event) {
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+		gulp.run('images');
+	});
 
 });
 
-gulp.task('default', ['sass', 'js', 'concat']);
+// Define Default Task
+gulp.task( 'default', [ 'styles', 'scripts', 'images' ] );
