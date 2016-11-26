cff53357bca19f08f4b3780a040847bab4b79a8c
Erik Bernskiold <erik@bernskioldmedia.com>
2016-11-26 13:20:52 +0100

    No more external links in new window

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,14 +1,2 @@
 // Load Foundation
 jQuery(document).foundation();
-
-// Open External Links in New Window
-jQuery('a').each(function() {
-   var a = new RegExp('/' + window.location.host + '/');
-   if(!a.test(this.href)) {
-       jQuery(this).click(function(event) {
-           event.preventDefault();
-           event.stopPropagation();
-           window.open(this.href, '_blank');
-       });
-   }
-});
