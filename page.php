f5d70967473457a27110b7e8ffe7c2e67ec6cf1c
Erik Bernskiold <erik@bernskioldmedia.com>
2017-09-24 20:27:05 +0200

    Update content file paths, fixes #3

--- page.php
+++ page.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'page' ); ?>
+			<?php get_template_part( 'components/page/content-page' ); ?>
 
 		<?php endwhile; ?>
 
